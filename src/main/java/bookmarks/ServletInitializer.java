c349ab11d3d7550ee7f51365fb65813b20040bba
Giau Ngo <giaunv@gmail.com>
2015-05-07 16:19:07 +0700

    Models and Repositories

--- src/main/java/bookmarks/ServletInitializer.java
+++ src/main/java/bookmarks/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(BookmarksApplication.class);
+		return application.sources(Application.class);
 	}
 
 }
